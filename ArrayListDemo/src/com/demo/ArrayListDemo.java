812c89e9c250dd46db7edeb5405524295243ebd2
Pooja <manepooja3129@gmail.com>
2023-02-11 12:08:58 +0530

    All Done

---
+++ ArrayListDemo/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList <String> l=new ArrayList <String>();
+		l.add("Pooja");
+		l.add("Priya");
+		l.add("Vaishu");
+		l.add("Samir");
+		
+		System.out.println(l);
+				
+
+	}
+
+}
